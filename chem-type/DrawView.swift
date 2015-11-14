4a6dbe31edf53ba1833f482a624ec37dde3a9e44
triki <triki@usc-secure-wireless-042-032.usc.edu>
2015-11-13 23:15:14 -0800

    Test2

---
+++ chem-type/DrawView.swift
@@ -0,0 +1,21 @@
+//
+//  DrawView.swift
+//  chem-type
+//
+//  Created by triki on 11/13/15.
+//  Copyright © 2015 triki. All rights reserved.
+//
+
+import UIKit
+
+class DrawView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
